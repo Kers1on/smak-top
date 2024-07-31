019045fb85b2c96744be1d522635c7cf016514d0
Kers1on <maximpro63@gmail.com>
2024-08-01 00:57:33 +0200

    feat: smallfix adding new dish

--- public/scripts/add-dish.js
+++ public/scripts/add-dish.js
@@ -6,64 +6,71 @@ document.getElementById('dishForm').addEventListener('submit', function(event) {
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
 
-    // Читаємо файл фото як base64
-    const reader = new FileReader();
-    reader.onloadend = function() {
-        const photoDataUrl = reader.result;
-
-        // Створюємо об'єкт JSON з зібраними значеннями
-        const dish = {
-            photo: photoDataUrl,
-            title: title,
-            description: description
-        };
-
-        // Відправляємо дані на сервер
-        fetch('/api/dishes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dish)
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Скидаємо форму
-            document.getElementById('dishForm').reset();
-            alert('Блюдо додано успішно!');
-        })
-        .catch(error => {
-            console.error('Помилка:', error);
-            alert('Сталася помилка при додаванні блюда.');
-        });
-    };
-
-    if (photo) {
-        reader.readAsDataURL(photo);
+    if (!title || !description) {
+        alert('Будь-ласка заповніть поля');
     } else {
-        // Створюємо об'єкт JSON без фото
-        const dish = {
-            title: title,
-            description: description
+        // Читаємо файл фото як base64
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            const photoDataUrl = reader.result;
+
+            // Створюємо об'єкт JSON з зібраними значеннями
+            const dish = {
+                photo: photoDataUrl,
+                title: title,
+                description: description
+            };
+
+            // Відправляємо дані на сервер
+            fetch('/api/dishes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dish)
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Скидаємо форму
+                document.getElementById('dishForm').reset();
+                alert('Блюдо додано успішно!');
+                window.location.href = 'index.html';
+            })
+            .catch(error => {
+                console.error('Помилка:', error);
+                alert('Сталася помилка при додаванні блюда.');
+            });
         };
 
-        // Відправляємо дані на сервер
-        fetch('/api/dishes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dish)
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Скидаємо форму
-            document.getElementById('dishForm').reset();
-            alert('Блюдо додано успішно!');
-        })
-        .catch(error => {
-            console.error('Помилка:', error);
-            alert('Сталася помилка при додаванні блюда.');
-        });
+        if (photo) {
+            reader.readAsDataURL(photo);
+        } else {
+            // Створюємо об'єкт JSON без фото
+            const dish = {
+                title: title,
+                description: description
+            };
+
+            // Відправляємо дані на сервер
+            fetch('/api/dishes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dish)
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Скидаємо форму
+                document.getElementById('dishForm').reset();
+                alert('Блюдо додано успішно!');
+                window.location.href = 'index.html';
+            })
+            .catch(error => {
+                console.error('Помилка:', error);
+                alert('Сталася помилка при додаванні блюда.');
+            });
+        }
     }
+
 });
